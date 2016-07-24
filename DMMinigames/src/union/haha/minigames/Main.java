5dd6f22ee4c15613058a58ad619df7bba9a6690e
HahaYouFell <AlonMC99@gmail.com>
2016-07-24 11:35:52 +0300

    Add files via upload
    DragonMiner's minigames plugin
    
    -Haha

--- DMMinigames/src/union/haha/minigames/Main.java
+++ DMMinigames/src/union/haha/minigames/Main.java
@@ -2,12 +2,17 @@ package union.haha.minigames;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -15,12 +20,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
+	public Game status;
+
 	public File minigame;
 	public FileConfiguration mgconf;
 
 	public int countdown = mgconf.getInt("countdown");
 
-	int playerswaiting;
+	public List<String> playerswaiting = new ArrayList<String>();
+
+	int amount_playerswaiting;
 
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
@@ -34,6 +43,8 @@ public class Main extends JavaPlugin implements Listener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		} else {
+			mgconf.options().copyDefaults(true);
 		}
 		if (!getDataFolder().exists()) {
 			getDataFolder().mkdirs();
@@ -42,14 +53,11 @@ public class Main extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
-		int x = -360;
-		int y = 223;
-		int z = 728;
 		Location waitingchunk = new Location(getServer().getWorld("flatroom"), mgconf.getInt("waitingroom_x"),
 				mgconf.getInt("waitingroom_y"), mgconf.getInt("waitingroom_z"));
 		if (e.getPlayer().getLocation().getChunk() == waitingchunk.getChunk()) {
-			playerswaiting++;
-			if (playerswaiting == 6) {
+			amount_playerswaiting++;
+			if (amount_playerswaiting == 6) {
 				getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 
 					@Override
@@ -66,9 +74,52 @@ public class Main extends JavaPlugin implements Listener {
 					// -HERE YOU START THE GAME-//
 					// -AND DO ALL THOSE-//
 
+					status = Game.started;
+					for (Player p : Bukkit.getOnlinePlayers()) {
+						if (playerswaiting.contains(p.getName())) {
+							p.teleport(new Location(getServer().getWorld(mgconf.getString("game_world")), mgconf.getInt("game_x"), mgconf.getInt("game_y"), mgconf.getInt("game_z")));
+						}
+					}
+					getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+
+						@Override
+						public void run() {
+							for  (Player p : Bukkit.getOnlinePlayers()) {
+								if (playerswaiting.contains(p.getName())) {
+									getServer().dispatchCommand(getServer().getConsoleSender(), "spawn " + p.getName());
+								}
+							}
+							
+						}
+						
+					}, 0, mgconf.getLong("game_time"));
+
 				}
 			}
 		}
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (label.equalsIgnoreCase("game")) {
+			if (args.length == 2) {
+				if (args[0].equalsIgnoreCase("join")) {
+					if (args[1].equalsIgnoreCase("battles")) {
+						if (amount_playerswaiting == 6) {
+							sender.sendMessage(ct("&cGame is full! Please wait for another game to start."));
+						} else if (amount_playerswaiting < 6) {
+							Player p = (Player) sender;
+							p.teleport(new Location(getServer().getWorld(mgconf.getString("waitingroom_world")), mgconf.getInt("waitingroom_x"), mgconf.getInt("waitingroom_y"), mgconf.getInt("waitingroom_z")));
+							p.sendMessage(ct("&aYou have been teleported to the game!"));
+							playerswaiting.add(p.getName());
+						}
+					}
+				} else {
+					sender.sendMessage(ct("&3Error: &1Invalid arguements, commands: /game join <game>(battles)"));
+				}
+			}
+		}
+		return false;
 	}
 
 	public String ct(String message) {
